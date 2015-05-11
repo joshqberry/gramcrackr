efd890b3cd8b6124fd2f133f7f6ebe907df9064b
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-05-11 16:20:48 -0600

    Add user image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.where(uid: auth_hash[:uid], token: auth_hash['credentials']['token'], name: auth_hash['info']['name']).first_or_create
+    @user = User.where(uid: auth_hash[:uid], token: auth_hash['credentials']['token'], name: auth_hash['info']['name'], image: auth_hash['info']['image']).first_or_create
     session[:user_id] = @user.id
     redirect_to users_path, notice: "Hey, it worked!"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511202249) do
+ActiveRecord::Schema.define(version: 20150511221318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150511202249) do
     t.string "uid"
     t.string "token"
     t.string "name"
+    t.string "image"
   end
 
 end
